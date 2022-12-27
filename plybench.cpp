7b68261b9145682ae94b18f74709c75db1d0c5d9
Ton van den Heuvel <git@tonvandenheuvel.com>
2022-12-27 12:03:55 +0100

    Add hapPLY benchmarks

--- plybench.cpp
+++ plybench.cpp
@@ -1,16 +1,70 @@
 #include "mesh.h"
-
+#include "submodules/happly/happly.h"
 #include "submodules/miniply/miniply.h"
 
 #include <plywoot/plywoot.hpp>
 
 #include <benchmark/benchmark.h>
 
+#include <algorithm>
+#include <array>
 #include <cstdint>
 #include <fstream>
 #include <vector>
 
-static void BM_plywoot(benchmark::State &state, const std::string &filename)
+static void BM_ParseHapply(benchmark::State &state, const std::string &filename)
+{
+  benchmark::ClobberMemory();
+
+  for (auto _ : state)
+  {
+    // Construct the data object by reading from file
+    happly::PLYData plyIn(filename);
+
+    if (!plyIn.hasElement("vertex"))
+    {
+      state.SkipWithError("could not load vertex data; vertex element not found in input data");
+    }
+
+    if (!plyIn.hasElement("face"))
+    {
+      state.SkipWithError("could not load triangle data; face element not found in input data");
+    }
+
+    // Convert hapPLY vertices to mesh vertices.
+    auto& vertexElement = plyIn.getElement("vertex");
+    std::vector<float> x = vertexElement.getProperty<float>("x");
+    std::vector<float> y = vertexElement.getProperty<float>("y");
+    std::vector<float> z = vertexElement.getProperty<float>("z");
+    if (x.size() != y.size() || x.size() != z.size())
+    {
+      state.SkipWithError("could not load vertex data; invalid vertex data found in input data");
+    }
+
+    std::vector<Vertex> vertices;
+    vertices.reserve(x.size());
+    for (size_t i = 0; i < x.size(); ++i)
+    {
+      vertices.push_back(Vertex{x[i], y[i], z[i]});
+    }
+
+    // Convert hapPLY triangles to mesh triangles.
+    std::vector<std::vector<std::int32_t>> happlyTriangles = plyIn.getFaceIndices<std::int32_t>();
+    std::vector<Triangle> triangles;
+    triangles.reserve(happlyTriangles.size());
+    std::transform(
+        happlyTriangles.begin(), happlyTriangles.end(), std::back_inserter(triangles),
+        [](const std::vector<std::int32_t> &t) {
+          return Triangle{t[0], t[1], t[2]};
+        });
+
+    // Finally, construct the mesh.
+    const TriangleMesh mesh{std::move(triangles), std::move(vertices)};
+    benchmark::DoNotOptimize(mesh);
+  }
+}
+
+static void BM_ParsePlywoot(benchmark::State &state, const std::string &filename)
 {
   benchmark::ClobberMemory();
 
@@ -48,7 +102,7 @@ static void BM_plywoot(benchmark::State &state, const std::string &filename)
   }
 }
 
-static void BM_miniply(benchmark::State &state, const std::string &filename)
+static void BM_ParseMiniply(benchmark::State &state, const std::string &filename)
 {
   benchmark::ClobberMemory();
 
@@ -57,9 +111,13 @@ static void BM_miniply(benchmark::State &state, const std::string &filename)
     const int verts_per_face = 3;
 
     miniply::PLYReader reader{filename.data()};
-    if (!reader.valid()) { state.SkipWithError((std::string{"could not open: "} + filename).data()); }
+    if (!reader.valid())
+    {
+      state.SkipWithError((std::string{"could not open: "} + filename).data());
+    }
 
-    miniply::PLYElement *facesElem = reader.get_element(reader.find_element(miniply::kPLYFaceElement));
+    miniply::PLYElement *facesElem =
+        reader.get_element(reader.find_element(miniply::kPLYFaceElement));
     if (!facesElem)
     {
       state.SkipWithError("could not load triangle data; face element not found in input data");
@@ -86,7 +144,8 @@ static void BM_miniply(benchmark::State &state, const std::string &filename)
         uint32_t propIdxs[3];
         if (!reader.find_pos(propIdxs)) { break; }
         mesh.vertices.resize(reader.num_rows());
-        reader.extract_properties(propIdxs, 3, miniply::PLYPropertyType::Float, mesh.vertices.data());
+        reader.extract_properties(
+            propIdxs, 3, miniply::PLYPropertyType::Float, mesh.vertices.data());
         gotVerts = true;
       }
       else if (!gotFaces && reader.element_is(miniply::kPLYFaceElement))
@@ -108,22 +167,37 @@ static void BM_miniply(benchmark::State &state, const std::string &filename)
   }
 }
 
-BENCHMARK_CAPTURE(BM_plywoot, "Asian Dragon (binary big endian)", "models/xyzrgb_dragon.ply");
-BENCHMARK_CAPTURE(BM_miniply, "Asian Dragon (binary big endian)", "models/xyzrgb_dragon.ply");
+BENCHMARK_CAPTURE(BM_ParseHapply, "Asian Dragon (binary big endian)", "models/xyzrgb_dragon.ply");
+BENCHMARK_CAPTURE(BM_ParseMiniply, "Asian Dragon (binary big endian)", "models/xyzrgb_dragon.ply");
+BENCHMARK_CAPTURE(BM_ParsePlywoot, "Asian Dragon (binary big endian)", "models/xyzrgb_dragon.ply");
 
-BENCHMARK_CAPTURE(BM_plywoot, "Lucy (binary big endian)", "models/lucy.ply");
-BENCHMARK_CAPTURE(BM_miniply, "Lucy (binary big endian)", "models/lucy.ply");
+BENCHMARK_CAPTURE(BM_ParseHapply, "Lucy (binary big endian)", "models/lucy.ply");
+BENCHMARK_CAPTURE(BM_ParseMiniply, "Lucy (binary big endian)", "models/lucy.ply");
+BENCHMARK_CAPTURE(BM_ParsePlywoot, "Lucy (binary big endian)", "models/lucy.ply");
 
-BENCHMARK_CAPTURE(BM_plywoot, "DOOM Combat Scene (binary little endian)", "models/Doom combat scene.ply");
-BENCHMARK_CAPTURE(BM_miniply, "DOOM Combat Scene (binary little endian)", "models/Doom combat scene.ply");
+BENCHMARK_CAPTURE(
+    BM_ParseHapply,
+    "DOOM Combat Scene (binary little endian)",
+    "models/Doom combat scene.ply");
+BENCHMARK_CAPTURE(
+    BM_ParseMiniply,
+    "DOOM Combat Scene (binary little endian)",
+    "models/Doom combat scene.ply");
+BENCHMARK_CAPTURE(
+    BM_ParsePlywoot,
+    "DOOM Combat Scene (binary little endian)",
+    "models/Doom combat scene.ply");
 
-BENCHMARK_CAPTURE(BM_plywoot, "Dragon (ASCII)", "models/dragon_vrip.ply");
-BENCHMARK_CAPTURE(BM_miniply, "Dragon (ASCII)", "models/dragon_vrip.ply");
+BENCHMARK_CAPTURE(BM_ParseHapply, "Dragon (ASCII)", "models/dragon_vrip.ply");
+BENCHMARK_CAPTURE(BM_ParseMiniply, "Dragon (ASCII)", "models/dragon_vrip.ply");
+BENCHMARK_CAPTURE(BM_ParsePlywoot, "Dragon (ASCII)", "models/dragon_vrip.ply");
 
-BENCHMARK_CAPTURE(BM_plywoot, "Happy Buddha (ASCII)", "models/happy_vrip.ply");
-BENCHMARK_CAPTURE(BM_miniply, "Happy Buddha (ASCII)", "models/happy_vrip.ply");
+BENCHMARK_CAPTURE(BM_ParseHapply, "Happy Buddha (ASCII)", "models/happy_vrip.ply");
+BENCHMARK_CAPTURE(BM_ParseMiniply, "Happy Buddha (ASCII)", "models/happy_vrip.ply");
+BENCHMARK_CAPTURE(BM_ParsePlywoot, "Happy Buddha (ASCII)", "models/happy_vrip.ply");
 
-BENCHMARK_CAPTURE(BM_plywoot, "Stanford bunny (ASCII)", "models/bun_zipper.ply");
-BENCHMARK_CAPTURE(BM_miniply, "Stanford bunny (ASCII)", "models/bun_zipper.ply");
+BENCHMARK_CAPTURE(BM_ParseHapply, "Stanford bunny (ASCII)", "models/bun_zipper.ply");
+BENCHMARK_CAPTURE(BM_ParseMiniply, "Stanford bunny (ASCII)", "models/bun_zipper.ply");
+BENCHMARK_CAPTURE(BM_ParsePlywoot, "Stanford bunny (ASCII)", "models/bun_zipper.ply");
 
 BENCHMARK_MAIN();
