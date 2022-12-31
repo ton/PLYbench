2174f73a46864263dba4a22a538ba9b7ad76be34
Ton van den Heuvel <git@tonvandenheuvel.com>
2022-12-31 14:50:12 +0100

    Add script to plot graph from benchmark CPU times

---
+++ scripts/plot_graph.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import json
+import matplotlib.pyplot
+import numpy
+import subprocess
+import sys
+
+benchmark_data = json.load(sys.stdin)
+benchmarks = benchmark_data['benchmarks']
+if not benchmarks:
+    sys.exit(1)
+
+# Mapping from a raw benchmark name to a human readable parser name.
+benchmark_parser_names = {'BM_ParseHapply' : 'hapPLY', 'BM_ParseMiniply' : 'miniply', 'BM_ParseMshPly' : 'msh_ply', 'BM_ParsePlywoot' : 'PLYwoot'}
+
+# Mapping from a parser name and model name to the time required to parse that
+# model by that parser.
+cpu_times_by_parser_and_model = {}
+time_unit = None
+
+# Create a mapping, from PLY file to parser benchmark results.
+for benchmark in benchmarks:
+    time_unit = benchmark['time_unit']
+
+    # Extract the model name, and the parser name.
+    benchmark_name, _, model_name = benchmark['name'].partition('/')
+
+    # Strip off the (" and ") pre- and postfix from the model name.
+    model_name = '\n('.join(model_name[2:-2].split(' ('))
+
+    cpu_times_by_parser_and_model[(benchmark_name, model_name)] = benchmark['cpu_time']
+
+dpi = 163
+width = 3840
+height = 2160
+
+bar_label_fontsize = 12
+legend_fontsize = 20
+title_fontsize = 28
+
+xlabel_fontsize = 20
+ylabel_fontsize = 28
+ticks_fontsize = 20
+
+bars_width = 0.85
+
+fig, ax = matplotlib.pyplot.subplots(figsize=(width / dpi, height / dpi))
+
+model_names = sorted(list(set([model_name for _, model_name in cpu_times_by_parser_and_model.keys()])), key=lambda n: tuple(reversed(n.split('\n'))))
+x = numpy.arange(len(model_names))
+
+bar_width = bars_width / len(benchmark_parser_names)
+offset = -0.5 * bars_width
+
+for benchmark_name, parser_name in benchmark_parser_names.items():
+    cpu_times = [cpu_times_by_parser_and_model[(benchmark_name, model_name)] for model_name in model_names]
+    rect = ax.bar(x + offset, cpu_times, bar_width, label=parser_name)
+    offset += bar_width
+    ax.bar_label(rect, fmt='%.2f', padding=3, fontsize=bar_label_fontsize)
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('CPU time [%s]' % time_unit, fontsize=ylabel_fontsize)
+ax.set_title('CPU time [%s] by model and parser' % time_unit, fontsize=title_fontsize)
+ax.set_xticks(x, model_names)
+ax.tick_params(axis='both', labelsize=ticks_fontsize)
+ax.legend(fontsize=legend_fontsize)
+
+fig.tight_layout()
+
+matplotlib.pyplot.savefig(sys.stdout, format='png', dpi=dpi)
