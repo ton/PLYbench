a6139ec191f2cfcfc41fb542e2b7a3602c49ea0d
Ton van den Heuvel <git@tonvandenheuvel.com>
2023-01-06 14:12:46 +0100

    Implement rendering transfer speed graphs

--- scripts/plot_graph.py
+++ scripts/plot_graph.py
@@ -20,16 +20,35 @@ ylabel_fontsize = 28
 
 bars_width = 0.90
 
-def render_graph(benchmarks, output_png_file, benchmark_metric, benchmark_parser_names, title_fn, ylabel_fn):
+# Mapping from a parse benchmark name to the associated human readable parser name.
+parse_benchmark_parser_names = { \
+    'BM_ParseHapply' : 'hapPLY', \
+    'BM_ParseMiniply' : 'miniply', \
+    'BM_ParseMshPly' : 'msh_ply', \
+    'BM_ParseNanoPly' : 'nanoply', \
+    'BM_ParsePlywoot' : 'PLYwoot', \
+    'BM_ParsePlyLib' : 'plylib', \
+    'BM_ParseRPly' : 'RPly', \
+    'BM_ParseTinyply' : 'tinyply 2.3'
+}
+
+# Mapping from a write benchmark name to the associated human readable parser name.
+write_benchmark_parser_names = { \
+    'BM_WriteHapply' : 'hapPLY', \
+    'BM_WriteMshPly' : 'msh_ply', \
+    'BM_WriteNanoPly' : 'nanoply', \
+    'BM_WritePlywoot' : 'PLYwoot', \
+    'BM_WriteRPly' : 'RPly', \
+    'BM_WriteTinyply' : 'tinyply 2.3' \
+}
+
+def render_graph(benchmarks, output_png_file, benchmark_metric, benchmark_parser_names, title, ylabel):
     # Mapping from a parser name and model name to the time required to parse that
     # model by that parser.
     cpu_times_by_parser_and_model = {}
-    time_unit = None
 
     # Create a mapping, from PLY file to parser benchmark results.
     for benchmark in benchmarks:
-        time_unit = benchmark['time_unit']
-
         # Extract the model name, and the parser name.
         benchmark_name, _, model_name = benchmark['name'].partition('/')
 
@@ -55,9 +74,9 @@ def render_graph(benchmarks, output_png_file, benchmark_metric, benchmark_parser
         offset += bar_width
         ax.bar_label(rect, fmt='%.2f', padding=3, fontsize=bar_label_fontsize, rotation=70)
 
-    ax.set_title(title_fn(time_unit), fontsize=title_fontsize)
+    ax.set_title(title, fontsize=title_fontsize)
     ax.set_xticks(x, model_names)
-    ax.set_ylabel(ylabel_fn(time_unit), fontsize=ylabel_fontsize)
+    ax.set_ylabel(ylabel, fontsize=ylabel_fontsize)
     ax.tick_params(axis='both', labelsize=ticks_fontsize)
     ax.legend(fontsize=legend_fontsize)
 
@@ -66,43 +85,47 @@ def render_graph(benchmarks, output_png_file, benchmark_metric, benchmark_parser
     matplotlib.pyplot.savefig(output_png_file, format='png', dpi=dpi)
 
 def render_parse_cpu_time_graph(benchmarks, output_png_file):
-    # Mapping from a raw benchmark name to a human readable parser name.
-    benchmark_parser_names = { \
-            'BM_ParseHapply' : 'hapPLY', \
-            'BM_ParseMiniply' : 'miniply', \
-            'BM_ParseMshPly' : 'msh_ply', \
-            'BM_ParseNanoPly' : 'nanoply', \
-            'BM_ParsePlywoot' : 'PLYwoot', \
-            'BM_ParsePlyLib' : 'plylib', \
-            'BM_ParseRPly' : 'RPly', \
-            'BM_ParseTinyply' : 'tinyply 2.3'
-    }
-
+    time_unit = None if not benchmarks else benchmarks[0]['time_unit']
     render_graph(benchmarks,
                  output_png_file,
                  'cpu_time',
-                 benchmark_parser_names,
-                 lambda time_unit: 'Average CPU time [%s] spent parsing various models by different PLY parser libraries' % time_unit,
-                 lambda time_unit: 'CPU time [%s]'
+                 parse_benchmark_parser_names,
+                 'Average CPU time [%s] spent parsing various models by different PLY parser libraries' % time_unit,
+                 'CPU time [%s]' % time_unit
     )
 
 def render_write_cpu_time_graph(benchmarks, output_png_file):
-    # Mapping from a raw benchmark name to a human readable parser name.
-    benchmark_parser_names = { \
-            'BM_WriteHapply' : 'hapPLY', \
-            'BM_WriteMshPly' : 'msh_ply', \
-            'BM_WriteNanoPly' : 'nanoply', \
-            'BM_WritePlywoot' : 'PLYwoot', \
-            'BM_WriteRPly' : 'RPly', \
-            'BM_WriteTinyply' : 'tinyply 2.3' \
-    }
-
+    time_unit = None if not benchmarks else benchmarks[0]['time_unit']
     render_graph(benchmarks,
                  output_png_file,
                  'cpu_time',
-                 benchmark_parser_names,
-                 lambda time_unit: 'Average CPU time [%s] spent writing random mesh data by different PLY parser libraries' % time_unit,
-                 lambda time_unit: 'CPU time [%s]' % time_unit
+                 write_benchmark_parser_names,
+                 'Average CPU time [%s] spent writing random mesh data per output PLY format type for different PLY parser libraries' % time_unit,
+                 'CPU time [%s]' % time_unit
+    )
+
+def render_parse_bytes_per_second_graph(benchmarks, output_png_file):
+    for benchmark in benchmarks:
+        benchmark['mib_per_second'] = benchmark['bytes_per_second'] / (1024 * 1024)
+
+    render_graph(benchmarks,
+                 output_png_file,
+                 'mib_per_second',
+                 parse_benchmark_parser_names,
+                 'Read performance [MiB/s] for different models and PLY parser libraries',
+                 'Read performance [MiB/s]'
+    )
+
+def render_write_bytes_per_second_graph(benchmarks, output_png_file):
+    for benchmark in benchmarks:
+        benchmark['mib_per_second'] = benchmark['bytes_per_second'] / (1024 * 1024)
+
+    render_graph(benchmarks,
+                 output_png_file,
+                 'mib_per_second',
+                 write_benchmark_parser_names,
+                 'Write performance [MiB/s] per output PLY format type for different PLY parser libraries',
+                 'Write performance [MiB/s]'
     )
 
 if __name__ == '__main__':
@@ -110,7 +133,7 @@ if __name__ == '__main__':
             prog='plot_graph.py',
             description='Plots various graphs given the JSON output generated by PLYbench.')
 
-    graph_type_choices = ['parse_cpu_time', 'write_cpu_time']
+    graph_type_choices = ['parse_cpu_time', 'write_cpu_time', 'parse_bytes_per_second', 'write_bytes_per_second']
 
     parser.add_argument('-i', '--input',
                         help='input JSON file generated by PLYbench, in case this is not specified, stdin is used instead')
@@ -136,3 +159,7 @@ if __name__ == '__main__':
             render_parse_cpu_time_graph(benchmarks, png_file)
         elif args.type == 'write_cpu_time':
             render_write_cpu_time_graph(benchmarks, png_file)
+        elif args.type == 'parse_bytes_per_second':
+            render_parse_bytes_per_second_graph(benchmarks, png_file)
+        elif args.type == 'write_bytes_per_second':
+            render_write_bytes_per_second_graph(benchmarks, png_file)
